87b5eca3ac23c1092ea6ae68c5dd5e8fe0cfcd8b
heyg <heyg@heyg.local>
2014-10-14 16:47:09 +0900

    トップ画面と作者画面の修正

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>FuelPHP Framework</title>
+		<title>Idea Community</title>
 		<?php echo Asset::css('bootstrap.min.css'); ?>
 		<?php echo Asset::css('bootstrap-custom.css'); ?>
 		<?php echo Asset::css('original.css'); ?>
@@ -10,21 +10,47 @@
 	<body>
 		<div class="container-fluid" id="container">
       <div class="scalein sidemenu" id="sidemenu">
-        <?php echo Asset::img('noimage.jpg',array('class'=>'logo')); ?>
+        <?php echo Asset::img('logo.png',array('class'=>'logo')); ?>
         <ul>
-          <li class="active"><a href="#"><span class="glyphicon glyphicon-star"></span> めにゅー１</a></li>
-          <li><a href="#"><span class="glyphicon glyphicon-star"></span> めにゅー２</a></li>
-          <li><a href="#"><span class="glyphicon glyphicon-star"></span> めにゅー３</a></li>
+          <li class="active"><a href="#"> Ｍｅｎｕ１</a></li>
+          <li><a href="#"> Ｍｅｎｕ２</a></li>
+          <li><a href="#"> Ｍｅｎｕ３</a></li>
         </ul>
         <div class="menu-bottom">
           <ul>
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> ろぐいん</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span>　Ｌｏｇｉｎ</a></li>
           </ul>
         </div>
       </div>
-			<?php echo $content; ?>
+      <div class="pusher">
+        <div class="col-md-12 column" id="content">
+          <div class="row">
+            <div class="col-xs-12" id="navbar-top">
+              <nav class="navbar navbar-default navbar-static-top" role="navigation">
+                <div class="container-fluid">
+                  <div class="navbar-header">
+                    <a class="navbar-brand" href="#" style="margin-left:15px;">IDECOM</a>
+                  </div>
+                  <div class="navbar-right">
+
+                    <button id="trigger" style="margin:3px 15px" class="btn btn-lg pull-right" data-effect="slidein"><span class="glyphicon glyphicon-th-list"></span> Menu</button>
+                  </div>
+                    <!-- <ul class="nav navbar-nav navbar-right">
+                      <li><object type="image/svg+xml" data="assets/img/facebook.svg"></object>
+                        <object type="image/svg+xml" data="assets/img/twitter.svg"></object>
+                        <object type="image/svg+xml" data="assets/img/github.svg"></object>
+                      </li>
+                    </ul> -->
+                </div>
+              </nav>
+            </div>
+            <?php echo $content; ?>
+          </div>
+        </div>
+      </div>
 		</div>
 	</body>
 </html>
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.js" type="text/javascript" charset="utf-8"></script>
+<?php echo Asset::js('bootstrap.js'); ?>
 <?php echo Asset::js('original.js'); ?>
--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -1,151 +1,164 @@
-<div class="pusher">
-	<div class="col-xs-12 column" id="content">
-		<div class="row">
-			<div class="col-xs-12">
-				<button id="trigger" class="btn btn-success btn-lg pull-right" data-effect="slidein">製作所</button>
-			</div>
-			<div class="col-xs-12" id="list">
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p><small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<div class="thumbnail">
-						<?php echo Asset::img('noimage.jpg'); ?>
-						<blockquote>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
-							</p> <small>さくひんたいとる</small>
-						</blockquote>
-					</div>
-				</div>
-			</div>
+<div class="col-xs-12 text-center">
+	<?php echo Asset::img('logo-bar.png',array('style'=>'max-width:100%;')); ?>
+</div>
+<div class="visible-sm visible-xs col-sm-12 text-center" id="filter-top">
+	<div class="container-fluid">
+		<ul class="list-inline">
+			<li><a href="">System</a></li>
+			<li><a href="">Image</a></li>
+			<li><a href="">Illust</a></li>
+		</ul>
+	</div>
+</div>
+<div class="col-md-9" id="list">
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p><small>さくひんたいとる</small>
+			</blockquote>
 		</div>
 	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-6 col-xs-12">
+		<div class="thumbnail">
+			<?php echo Asset::img('noimage.jpg'); ?>
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur  adipisicing elit.
+				</p> <small>さくひんたいとる</small>
+			</blockquote>
+		</div>
+	</div>
+</div>
+<div class="hidden-sm hidden-xs col-md-3" id="filter-side">
+	<h2 class="text-center">Filter</h2>
+	<ul class="list-unstyled">
+		<li><a href="">System</a></li>
+		<li><a href="">Web</a></li>
+		<li><a href="">Movie</a></li>
+		<li><a href="">Image</a></li>
+		<li><a href="">Illust</a></li>
+	</ul>
 </div>
