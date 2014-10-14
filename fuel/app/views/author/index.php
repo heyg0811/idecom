e46ba17b5da5c0b3861b6fda8c112dc1e513ca23
b1225 <b1225@8A11.oic.jp>
2014-10-14 18:51:43 +0900

    author add

---
+++ fuel/app/views/author/index.php
@@ -0,0 +1,73 @@
+<div class="pusher">
+  <div class="col-xs-12 column" id="content">
+    <div class="col-sm-4 col-md-4 col-xs-12 temp author">
+      <?php echo Asset::img('noimage.jpg', array('class' => 'author-thumbnail')); ?>
+      <div class="temp">
+        <div class="">
+          <div class="row">
+            <div class="col-xs-4 author-grade">学年</div><div class="col-xs-4 author-grade">XXXXXXXX</div><br>
+            <div class="col-xs-4 author-grade">専攻</div><div class="col-xs-4 author-grade">XXXXXXXX</div><br>
+            <div class="col-xs-4 author-grade">スキル</div><br>
+            <div class="col-xs-4 author-grade">XXXXXXXX</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-7 col-md-7 col-xs-12 timeline">
+      <div class="jumbotron">
+        as;kljfaslkjdflksajdlfkasdlkfjas;ldfj;laskdjflk;
+      </div>
+      <div class="jumbotron">
+        as;kljfaslkjdflksajdlfkasdlkfjas;ldfj;laskdjflk;
+      </div>
+      <div class="jumbotron">
+        as;kljfaslkjdflksajdlfkasdlkfjas;ldfj;laskdjflk;
+      </div>
+      <div class="jumbotron">
+        as;kljfaslkjdflksajdlfkasdlkfjas;ldfj;laskdjflk;
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+<style>
+  
+/*                      author css                                      */
+
+img.author-thumbnail {
+    width: 150px;
+    height: 150px;
+    
+}
+.author
+{
+    margin-top: 50px;
+    margin-left: 50px;
+    
+}
+@media screen and (max-width: 768px) {
+    /* 768px以下の場合 */
+    .author
+    {
+      margin-left: 0;
+    }
+    img.author-thumbnail
+    {
+      float: left;
+    }
+}
+.author-grade
+{
+}
+.timeline
+{
+    margin-top: 50px;
+}
+.temp
+{
+    background-color: #36393B;
+}
+
+</style>
