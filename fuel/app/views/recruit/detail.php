8e666a35319a65e29e684dcf3769c749dcc41258
oic <b1039@oic.jp>
2014-10-21 15:47:57 +0900

    bosyu_syosai_totyu

--- fuel/app/views/recruit/detail.php
+++ fuel/app/views/recruit/detail.php
@@ -1,151 +1,47 @@
-<div class="col-sm-6 col-xs-12">
-  <div class="col-xs-12" id="product">
-    <div class="col-xs-12">
-      <div class="small-box bg-red">
-        <?php echo Asset::img('noimage.jpg'); ?>
-        <div class="inner">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus in eius blanditiis doloribus
-          </p>
+ <style type="text/css">
+    <!--
+    img{
+      width:300px;
+      height: 300px;
+    }
+    .importText {
+      text-decoration:underline;
+    }
+
+    .hero-unit{
+      background-color:#FAFCE0;
+    }
+    -->
+    </style>
+
+
+    <div row>
+      <div class="col-md-4 col-sm-6 col-xs-12" >
+        <div class="thumbnail">
+          <?php echo Asset::img('noimage.jpg'); ?>
         </div>
-        <a href="#" class="small-box-footer">
-          More info <i class="fa fa-arrow-circle-right"></i>
-        </a>
+          <br>
+          <br>
+            <span class ="importText">
+              <p>
+                title:
+              </p>
+            </span>
+            <p>
+              <span class ="importText">
+                募集スキル
+              </span>
+              <br>&emsp;・
+              <br>&emsp;・
+              <br>
+            </p>
+          <span class ="importText">
+            <p>
+              募集者名:
+            </p>
+          </span>
       </div>
-    </div>
+    <div class="col-md-8 col-sm-6 col-xs-12" id="">
+      <pre class="hero-unit">詳細内容<pre>
+
   </div>
-  <div class="col-xs-12" id="product-img">
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6">
-      <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="col-sm-6 col-xs-12" id="product-etc">
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-  <div class="col-md-4 col-sm-6 col-xs-12">
-    <div class="thumbnail">
-      <?php echo Asset::img('noimage.jpg'); ?>
-    </div>
-  </div>
-</div>
