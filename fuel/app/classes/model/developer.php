a41ba3df56bbe376a03b0bda611d0c3d8e7be560
b1225 <b1225@8A11.oic.jp>
2014-11-07 17:03:18 +0900

    author/edit・detail　更新

--- fuel/app/classes/model/developer.php
+++ fuel/app/classes/model/developer.php
@@ -5,90 +5,55 @@
  *
  * @author b1225
  */
-class Model_Developer extends Orm\Model {
+class Model_Developer extends \Orm\Model {
 
+  // テーブル情報を設定
   protected static $_table_name = 'developer';
   protected static $_properties = array(
-      'DeveloperId',
-      'NickName',
-      'Address',
-      'Grade',
-      'Major',
-      'Technology',
+      'id',
+      'user_id',
+      'nickname',
+      'address',
+      'grade',
+      'major',
+      'technology',
   );
-  protected static $_primariy = array('DeveloperId');
+  protected static $_primary_key = array('id');
 
-  //developerテーブルにinsert
-  public static function insert($user) {
-    $column = array('NickName', 'Address', 'Grade', 'Mojor', 'Technology');
-
-    \DB::insert("developer")
-            ->columns($column)
-            ->values($user)
-            ->execute();
-  }
-
-  //developer情報の更新
-  public static function updata($deve_id, $deve) {
-    \DB::update("developer")
-            ->set(array(
-                'NickName' => $deve['NickName'],
-                'Address' => $deve['Address'],
-                'Grade' => $deve['Grade'],
-                'Major' => $deve['Major'],
-                'Technology' => Model_Developer::technology_encode($deve['Technology']),
-            ))
-            ->where("DeveloperId", $deve_id)
-            ->execute();
-  }
-
-  //developer検索
-  public static function find($id = null, array $options = array()) {
-
-    //全件検索
-    if ($id === 'all') {
-      $result = DB::select('*')
-              ->from('Developer')
-              ->execute()
-              ->as_array();
-      foreach ($result as $key => $val) {
-        $val['Technology'] = Model_Developer::technology_decode($val['Technology']);
-      }
-    } else {
-      //１件検索
-      $result = DB::select('*')
-              ->from('Developer')
-              ->where('DeveloperId', $id)
-              ->execute()
-              ->as_array();
-      $result[0]['Technology'] = Model_Developer::technology_decode($result[0]['Technology']);
-    }
-
-    return $result;
-  }
-
-  //technologyをjson形式にencode
+  /**
+   * @brif    入力チェック
+   * @access  private
+   * @return
+   */
+//technologyをjson形式にencode
   public static function technology_encode($technology) {
     return json_encode($technology);
   }
 
-  //technologyをjson形式からdecode
+//technologyをjson形式からdecode
   public static function technology_decode($technology) {
     return json_decode($technology);
   }
 
-  public static function developerValidate() {
+  public static function validate() {
     $validation = Validation::forge();
-    $validation->add('NickName', 'ユーザー名')
+    $validation->add('nickname', '名前')
             ->add_rule('min_length', 2)
             ->add_rule('max_length', 50);
-    $validation->add('Address', 'Eメール')
+    $validation->add('address', 'メール')
             ->add_rule('valid_email');
-    $validation->add('Grade', '学年')
-            ->add_rule('exact_length', 1);
-    $validation->add('Major', '専攻')
+    $validation->add('grade', '学年')
+            ->add_rule('exact_length', 1)
+            ->add_rule('numeric_min', 1)
+            ->add_rule('numeric_max', 4)
+            ->add_rule('numeric');
+    $validation->add('major', '専攻')
             ->add_rule('max_length', 50);
-    $validation->run();
+    if (!$validation->run()) {
+      //失敗
+      $errors = $validation->error();
+      return $errors;
+    }
     return $validation;
   }
 
--- fuel/app/views/author/edit.php
+++ fuel/app/views/author/edit.php
@@ -1,63 +1,62 @@
 <!-- general form elements -->
+<?php if (!empty($errmsg)): ?>
+  <?php echo $errmsg; ?>
+<?php endif; ?>
 <div class="box box-success">
   <!-- form start -->
   <form role="form">
-    <?php foreach ($developer as $val): ?>
-      <?php echo \Form::csrf(); ?>
-      <div class="box-body">
-        <div class="form-group">
-          <label for="inputName">名前</label>
-          <input type="text" class="form-control" id="inputName" name="NickName" placeholder="名前" value="<?php echo $val['NickName']; ?>">
+    <?php echo \Form::csrf(); ?>
+    <div class="box-body">
+      <div class="form-group">
+        <label for="inputName">名前</label>
+        <input type="text" class="form-control" id="inputName" name="nickname" placeholder="名前" value="<?php echo $developer['nickname']; ?>">
+      </div>
+      <div class="form-group">
+        <label for="inputEmail">メール</label>
+        <input type="email" class="form-control" id="inputEmail" name="address" placeholder="メール" value="<?php echo $developer['address']; ?>">
+      </div>
+      <div class="form-group row">
+        <div class="col-xs-3">
+          <label for="inputGrade">学年</label>
         </div>
-        <div class="form-group">
-          <label for="inputEmail">メール</label>
-          <input type="email" class="form-control" id="inputEmail" name="Address" placeholder="メール" value="<?php echo $val['Address']; ?>">
+        <div class="col-xs-3">
+          <input type="text" class="form-control" id="inputGrade" name="grade" placeholder="学年" value="<?php echo $developer['grade']; ?>">
         </div>
-        <div class="form-group row">
-          <div class="col-xs-3">
-            <label for="inputGrade">学年</label>
-          </div>
-          <div class="col-xs-3">
-            <input type="text" class="form-control" id="inputGrade" name="Grade" placeholder="学年" value="<?php echo $val['Grade']; ?>">
-          </div>
-          <div class="col-xs-3">
-            <label for="inputMajor">専攻</label>
-          </div>
-          <div class="col-xs-3">
-            <input type="text" class="form-control" id="inputMajor" name="Major" placeholder="専攻" value="<?php echo $val['Major']; ?>">
-          </div>
+        <div class="col-xs-3">
+          <label for="inputMajor">専攻</label>
         </div>
+        <div class="col-xs-3">
+          <input type="text" class="form-control" id="inputMajor" name="major" placeholder="専攻" value="<?php echo $developer['major']; ?>">
+        </div>
+      </div>
 
-        <div class="form-group">
-          <label for="exampleInputFile">サムネイル</label>
-          <input type="file" id="exampleInputFile">
-        </div>
-      </div><!-- /.box-body -->
-      <hr>
-      <div class="box-body">
-        <?php foreach (Config::get('TECHNOLOGY') as $key => $val): ?>
-          <?php if ($key % 6 == 0): ?>
-            <div class="row margin-bottom15">
-            <?php endif; ?>
-            <div class="col-xs-1 text-center ">
-              <label><?php echo $val; ?></label>
-            </div>
-            <div class="col-xs-1 ">
-              <input type="hidden" name="skil_name[]" value="<?php echo $val; ?>">
-              <?php if (isset($developer[0]["Technology"][$key])): ?>
-                <input type="text" name="skil[]" size="5" maxlength="3" value="<?php echo $developer[0]["Technology"][$key]; ?>">
-              <?php endif; ?>
-            </div>
-            <?php if (($key % 6) === 5): ?>
-            </div>
+      <div class="form-group">
+        <label for="exampleInputFile">サムネイル</label>
+        <input type="file" id="exampleInputFile">
+      </div>
+    </div><!-- /.box-body -->
+    <hr>
+    <div class="box-body">
+      <?php foreach (Config::get('TECHNOLOGY') as $key => $val): ?>
+        <?php if ($key % 6 == 0): ?>
+          <div class="row margin-bottom15">
           <?php endif; ?>
-        <?php endforeach; ?>
-      </div>
-      <div class="box-footer">
-        <button type="submit" class="btn btn-success">更新</button>
-      </div>
-  </div>
-<?php endforeach; ?>
+          <div class="col-xs-1 text-center ">
+            <label><?php echo $val; ?></label>
+          </div>
+          <div class="col-xs-1 ">
+            <input type="hidden" name="skil_name[]" value="<?php echo $val; ?>">
+            <input type="text" name="skil[]" size="5" maxlength="3" value="<?php echo $developer["technology"][$key]; ?>">
+          </div>
+          <?php if (($key % 6) === 5): ?>
+          </div>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    </div>
+    <div class="box-footer">
+      <button type="submit" class="btn btn-success">更新</button>
+    </div>
+</div>
 
 </form>
 </div><!-- /.box -->
