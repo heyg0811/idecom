5452702d8db5e95229cc006f8da439a0484f72c1
JohnDoe <JohnDoe@JohnDoe-no-MacBook-Pro.local>
2014-11-08 11:23:03 +0900

    bbs_thread,bbs_comment 編集

--- fuel/app/classes/controller/bbs.php
+++ fuel/app/classes/controller/bbs.php
@@ -38,25 +38,25 @@ class Controller_Bbs extends Controller_Template {
   }
 
 
-  public function action_list() {
-    $this->template->subtitle = 'スレッド一覧';
-    $this->template->content = View::forge('bbs/list');
-  }
-
-
   public function action_thread() {
-    $this->template->subtitle = 'スレッド詳細';
+    $this->template->subtitle = 'スレッド一覧';
     $this->template->content = View::forge('bbs/thread');
     // 初期表示時
 
-    //thread_idを取得 1はテストデータ
-    $get_thread = 1;
-    //取得したthread_idと同じthrea_idのものをshow_dataに格納
-    $show_data = Model_Bbsthread::find('last', array('select' => array('date', 'user_id','comment'),
-                                                     'where' => array(array('thread_id', $get_thread),),
-                                                     'order_by' => array('date' => 'desc'), ));
+    //threadテーブルからデータ取得
+    $threads = Model_Bbsthread::find('all', array(
+      'select' => array('user_id', 'title','date'),
+      'order_by' => array('date' => 'desc'),
+     ));
 
-    //var_dump($show_data);exit();
+    $this->template->content->threads = $threads;
+    //commentテーブルから最初のコメントを取得
+    $first_comment = Model_Bbscomment::find('all', array(
+      'select' => array('comment'),
+      'where' => array(array('comment_id', 1),),
+     ));
+    $this->template->content->first_comment = $first_comment;
+
     if (!Security::check_token())
     {
       return ;
@@ -67,14 +67,58 @@ class Controller_Bbs extends Controller_Template {
     $validation = Model_Bbsthread::validate();
     $thread_data = $validation->validated();
     $bbsthread_model = new Model_Bbsthread();
+    $bbscomment_model = new Model_Bbscomment();
     $bbsthread_model->set(array(
-      //threa_idはテストデータ
-                          'thread_id'  => 1/*$thread_id['thread_id']*/,
                           'user_id'  => 'B1123547',
-                          'comment' => $thread_data['comment'],
+                          'title' => $thread_data['title'],
                           'date' => date("Y/m/d H:i:s"),
                           ));
+
+    $bbscomment_model->set(array(
+                          'comment' => $thread_data['comment'],
+                          ));
     $bbsthread_model->save();
+    $bbscomment_model->save();
+
+    }
+
+//---------------------------ここからコメント一覧のコード---------------------------------------
+  public function action_comment() {
+    $this->template->subtitle = 'レス一覧';
+    $this->template->content = View::forge('bbs/comment');
+    // 初期表示時
+
+    //thread_idを取得
+    $get_thread = 11;
+
+    //取得したthread_idと同じthrea_idのものをshow_dataに格納
+    $comments = Model_Bbscomment::find('all', array(
+      'select' => array('date', 'user_id','comment'),
+      'where' => array(array('thread_id', $get_thread),),
+      'order_by' => array('date' => 'desc'),
+     ));
+
+    $this->template->content->comments = $comments;
+
+
+    if (!Security::check_token())
+    {
+      return ;
+    }
+
+    //$bbslist_model = new Model_Bbslist();
+    //$thread_id = listテーブルのthread_id;
+    $validation = Model_Bbscomment::validate();
+    $comment_data = $validation->validated();
+    $bbscomment_model = new Model_Bbscomment();
+    $bbscomment_model->set(array(
+      //threa_idはテストデータ
+                          'thread_id'  => 11/*今いるスレッドのthread_idの値*/,
+                          'user_id'  => 'B1123547',
+                          'comment' => $comment_data['comment'],
+                          'date' => date("Y/m/d H:i:s"),
+                          ));
+    $bbscomment_model->save();
 
   }
 
--- fuel/app/views/bbs/thread.php
+++ fuel/app/views/bbs/thread.php
@@ -1,34 +1,71 @@
-<div class="box box-info">
-  <div class="box-header">
-    <i class="fa fa-comments-o"></i>
-    <h3 class="box-title">スレッドタイトル</h3>
+<style>
+  .thread:hover{
+    opacity:0.5;
+  }
+</style>
+<?php foreach ($comments as $comment ): ?>
+  <div class="row" style="margin:10px;">
+    <div class="col-sm-12">
+      <a href="/bbs/thread">
+      <div class="panel box box-info thread">
+        <div class="row">
+          <div class="col-sm-4">
+            <?php echo Asset::img('noimage.jpg', array('class' => 'img-responsive thread-img')); ?>
+          </div>
+          <div class="col-sm-7 bbs-box">
+            <div class="box-header">
+              <div class="box-title">
+                <i class="fa fa-fw fa-comment-o"></i><?php echo $comment['user_id'];  echo $i ?>
+              </div>
+            </div>
+            <div class="panel-collapse collapse in">
+              <div class="body">
+                <p <small class="text-muted pull-right"><i class="fa fa-clock-o"></i>
+                   <?php echo $comment['date']; ?>
+                   </small>class="text-black">
+                   <?php echo $comment['comment']; ?>
+               </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
   </div>
-  <div class="box-body chat" id="chat-box">
-    <?php for ($i = 0; $i < 20; $i++): ?>
-      <hr />
-      <div class="item">
-        <img src="img/avatar.png" alt="user image" class="online"/>
-        <p class="message">
-          <a href="/author/detail" class="name">
-            <small class="text-muted pull-right"><i class="fa fa-clock-o"></i> 2:15</small>
-            ゆーざー
-          </a>
-          本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん本文ほんぶん
-        </p>
-      </div><!-- /.item -->
-    <?php endfor ?>
-  </div><!-- /.chat -->
-
-  <form action="thread" method="post">
-    <?php echo \Form::csrf(); ?>
-    <div class="box-footer">
+<?php endforeach;?>
+<div class="box">
+  <div class="box-header">
+    <div class="box-title">
+      <p>スレッド投稿</p>
+    </div>
+  </div>
+  <form role="form">
+    <div class="box-body">
       <div class="form-group">
-        <label for="">レス</label>
-        <textarea name="comment" class="form-control" rows="5" ></textarea>
+        <label for="">タイトル</label>
+        <input class="form-control" type="text" name="title">
+      </div>
+      <div class="form-group">
+        <label for="">スレッド</label>
+        <textarea class="form-control" rows="10" ></textarea>
       </div>
     </div>
     <div class="box-footer">
-      <input type="submit" value="投稿" class="btn btn-success btn-lg">
+      <button class="btn btn-success btn-lg">投稿</button>
     </div>
   </form>
-</div><!-- /.box (chat box) -->
+</div>
+<style>
+  .bbs-box
+  {
+    margin: 10px -10px 10px 10px;
+  }
+  @media screen and (max-width: 991px) {
+    /* 991px以下の場合 */
+    .bbs-box
+    {
+      margin: 10px;
+    }
+  }
+</style>
