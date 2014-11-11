aeb3b4d07e6472497097c86877a9caab4bc3f1ea
b1225 <b1225@8A11.oic.jp>
2014-11-11 18:25:02 +0900

    db 追加

---
+++ document/user_1.sql
@@ -0,0 +1,156 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# バージョン 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# ホスト: 127.0.0.1 (MySQL 5.6.21)
+# データベース: idecom_db
+# 作成時刻: 2014-11-07 10:14:38 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# テーブルのダンプ user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `group` int(11) NOT NULL DEFAULT '1',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `login_hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `profile_fields` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) unsigned NOT NULL,
+  `updated_at` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# テーブルのダンプ user_clients
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_clients`;
+
+CREATE TABLE `user_clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL DEFAULT '',
+  `client_id` varchar(32) NOT NULL DEFAULT '',
+  `client_secret` varchar(32) NOT NULL DEFAULT '',
+  `redirect_uri` varchar(255) NOT NULL DEFAULT '',
+  `auto_approve` tinyint(1) NOT NULL DEFAULT '0',
+  `autonomous` tinyint(1) NOT NULL DEFAULT '0',
+  `status` enum('development','pending','approved','rejected') NOT NULL DEFAULT 'development',
+  `suspended` tinyint(1) NOT NULL DEFAULT '0',
+  `notes` tinytext NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# テーブルのダンプ user_providers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_providers`;
+
+CREATE TABLE `user_providers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL DEFAULT '0',
+  `provider` varchar(50) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `secret` varchar(255) DEFAULT NULL,
+  `access_token` varchar(255) DEFAULT NULL,
+  `expires` int(12) DEFAULT '0',
+  `refresh_token` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `created_at` int(11) NOT NULL DEFAULT '0',
+  `updated_at` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# テーブルのダンプ user_scopes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_scopes`;
+
+CREATE TABLE `user_scopes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `scope` varchar(64) NOT NULL DEFAULT '',
+  `name` varchar(64) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `scope` (`scope`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# テーブルのダンプ user_sessions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_sessions`;
+
+CREATE TABLE `user_sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` varchar(32) NOT NULL DEFAULT '',
+  `redirect_uri` varchar(255) NOT NULL DEFAULT '',
+  `type_id` varchar(64) NOT NULL,
+  `type` enum('user','auto') NOT NULL DEFAULT 'user',
+  `code` text NOT NULL,
+  `access_token` varchar(50) NOT NULL DEFAULT '',
+  `stage` enum('request','granted') NOT NULL DEFAULT 'request',
+  `first_requested` int(11) NOT NULL,
+  `last_updated` int(11) NOT NULL,
+  `limited_access` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `oauth_sessions_ibfk_1` (`client_id`),
+  CONSTRAINT `oauth_sessions_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `user_clients` (`client_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# テーブルのダンプ user_sessionscopes
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_sessionscopes`;
+
+CREATE TABLE `user_sessionscopes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) NOT NULL,
+  `access_token` varchar(50) NOT NULL DEFAULT '',
+  `scope` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `session_id` (`session_id`),
+  KEY `access_token` (`access_token`),
+  KEY `scope` (`scope`),
+  CONSTRAINT `oauth_sessionscopes_ibfk_1` FOREIGN KEY (`scope`) REFERENCES `user_scopes` (`scope`),
+  CONSTRAINT `oauth_sessionscopes_ibfk_2` FOREIGN KEY (`session_id`) REFERENCES `user_sessions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
