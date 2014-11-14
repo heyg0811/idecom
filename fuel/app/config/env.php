e40e23c3767a9cce4c95d54e0a736f32f76bb941
heyg <heyg@heyg.local>
2014-11-14 15:34:53 +0900

    b1039 googleIDとsercretの追加

--- fuel/app/config/env.php
+++ fuel/app/config/env.php
@@ -36,27 +36,43 @@ return array(
   * Google ClientID ClientSercret
   */
 
-  // idecom.heyg.pw
+  /*
+   *  idecom.heyg.pw
+   */
   'GOOGLE' => array(
     'CLIENTID' => '659226090229-v9gvnai3n3gm5qsefoh1e186jfj0em0n.apps.googleusercontent.com',
     'CLIENTSECRET' => 'e_3cxYRoIS5P0_yHFTKXB0U2',
   ),
 
-  // localhost
+  /*
+   *  localhost
+   */
   // 'GOOGLE' => array(
   //   'CLIENTID' => '659226090229-v67usoibjvhigp7i6uugb4vso0fmvunf.apps.googleusercontent.com',
   //   'CLIENTSECRET' => '218i4jzZstmafrKms_4GWD2F',
   // ),
 
-  // heyg0811 CloudIDE
+  /*
+   *  heyg0811 CloudIDE
+   */
   // 'GOOGLE' => array(
   //   'CLIENTID' => '659226090229-vv1u3ink69c3ovuqd84sv3bo1bn2dlil.apps.googleusercontent.com',
   //   'CLIENTSECRET' => 'b5l4lGWK8QiZrNyBfd94RIvH',
   // ),
 
-  // b1225 CloudIDE
+  /*
+   *  b1225 CloudIDE
+   */
   // 'GOOGLE' => array(
   //   'CLIENTID' => '659226090229-imtdu5irndlqtvm7tprm6defh7lc546b.apps.googleusercontent.com',
   //   'CLIENTSECRET' => 'TL-cWOrVFINIFLJMwAOBIrAt',
+  // ),
+
+  /*
+   *  b1039 CloudIDE
+   */
+  // 'GOOGLE' => array(
+  //   'CLIENTID' => '659226090229-iq9h2f2pfblcmbvl7rf3kjlp70mlkerf.apps.googleusercontent.com',
+  //   'CLIENTSECRET' => 'ZfVT9u8kdOU8pqaL48yapY9t',
   // ),
 );
