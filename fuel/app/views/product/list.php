03fbc45c970f285ad483c94dcf950defe9a1f2de
kamohasu <b1039@oic.jp>
2015-01-19 12:06:49 +0000

    product lidt

--- fuel/app/views/product/list.php
+++ fuel/app/views/product/list.php
@@ -20,7 +20,7 @@
           <img src="<?php echo Config::get('UPLOAD_URL') . $product['thumbnail']; ?>" class="img-responsive">
           <div class="inner">
             <h4>
-              <i class="fa fa-desktop" style="margin-right:4px;"></i> さくひんたいとる
+              <i class="fa fa-desktop" style="margin-right:4px;"></i><?php echo $product['title']; ?>
             </h4>
             <p style="height:100px;">
               <?php echo $product['outline'];?>
